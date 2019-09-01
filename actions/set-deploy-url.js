41e7a44f0b0885866fb8ee0573f33e2625bd8374
Jasper Moelker <j.b.moelker@activeids.nl>
2019-09-01 14:25:27 +0200

    Add workflow step to set deploy url

---
+++ actions/set-deploy-url.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core')
+const fs = require('fs')
+
+function getEventData() {
+  return JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'))
+}
+
+const eventData = getEventData()
+const prNumber = eventData.check_suite.pull_requests[0].number
+const deployUrl = `https://deploy-preview-${prNumber}--demo-actions-on-netlify.netlify.com/`
+
+core.debug(`deploy url: ${deployUrl}`)
+
+// https://github.com/actions/toolkit/tree/master/packages/core#exporting-variables
+core.exportVariable('DEPLOY_URL', deployUrl)
