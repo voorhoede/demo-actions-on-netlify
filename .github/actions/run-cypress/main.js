8e87a4449d4521a412024a3bff79e49317781e85
Selwyn <talk@selwyn.cc>
2019-09-13 15:04:21 +0200

    🚧🚧🚧 IIII

--- .github/actions/run-cypress/main.js
+++ .github/actions/run-cypress/main.js
@@ -7,35 +7,42 @@ const core = require('@actions/core');
 const { exec } = require('@actions/exec');
 const toolCache = require('@actions/tool-cache');
 
-try {
-  main();
-} catch (error) {
-  core.setFailed(error.message);
-}
+const baseUrl = core.getInput('base-url', { required: true });
 
-function main() {
-  const baseUrl = core.getInput('base-url', { required: true });
+const cypress = {
+  version: JSON.parse(fs.readFileSync('package-lock.json')).dependencies.cypress.version,
+  cachePath: path.join(process.env.GITHUB_WORKSPACE, 'cypress-cache'),
+};
 
-  const cypressCachePath = path.join(process.env.GITHUB_WORKSPACE, 'cypress-cache');
-  const cypressVersion = JSON.parse(fs.readFileSync('package-lock.json')).dependencies.cypress.version;
+const runEnvironment = {
+  ...process.env,
+  'CI': 'true',
+  'CYPRESS_CACHE_FOLDER': cypress.cachePath,
+};
 
-  core.exportVariable('CYPRESS_CACHE_FOLDER', cypressCachePath);
+getCypress(cypress.version)
+  .then(() => {
+    toolCache.cacheDir(
+      cypress.cachePath,
+      'cypress',
+      cypress.version
+    )
 
-  getCypress(cypressVersion)
-    .then(() => {
-      toolCache.cacheDir(
-        cypressCachePath,
-        'cypress',
-        cypressVersion
-      )
-
-      exec(`./node_modules/.bin/cypress run --config baseUrl=${baseUrl}`);
-    });
-}
+    return exec(
+      './node_modules/.bin/cypress',
+      ['run', '--config', `baseUrl=${baseUrl}`],
+      { env: runEnvironment }
+    )
+  })
+  .catch(error => core.setFailed(error.message));
 
 function getCypress(version) {
   if (toolCache.find('cypress', version) !== '')
     return;
 
-  return exec('npm install --no-audit cypress');
+  return exec(
+    'npm',
+    ['install', '--no-audit', 'cypress'],
+    { env: runEnvironment }
+  );
 }
