da06855b550708c338a1dc8f9c07e7833b942401
Selwyn <talk@selwyn.cc>
2020-04-21 13:51:37 +0200

    Initial Cypress setup

--- .github/actions/get-netlify-url/main.js
+++ .github/actions/get-netlify-url/main.js
@@ -4,22 +4,22 @@ const fs = require('fs');
 
 const core = require('@actions/core');
 
-try {
-  main();
-} catch (error) {
-  core.setFailed(error.message);
-}
+const eventData = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH));
+const pullRequestNumber = eventData.check_suite.pull_requests[0].number;
 
-function main() {
-  const eventData = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH));
-  const pullRequestNumber = eventData.check_suite.pull_requests[0].number;
-  const siteName = core.getInput('site-name') || eventData.repository.name;
+const siteName = core.getInput('site-name') || eventData.repository.name;
+const basicAuthUsername = core.getInput('basic-auth-username');
+const basicAuthPassword = core.getInput('basic-auth-password');
+const basicAuth = basicAuthUsername && basicAuthPassword
+  ? `${basicAuthUsername}:${basicAuthPassword}@`
+  : '';
 
-  core.debug(`Context: ${pullRequestNumber ? 'pull request' : 'none'}`);
-  core.debug(`Site name: ${siteName}`);
+core.debug(`Context: ${pullRequestNumber ? 'pull request' : 'none'}`);
+core.debug(`Site name: ${siteName}`);
 
-  core.setOutput(
-    'url',
-    `https://deploy-preview-${pullRequestNumber}--${siteName}.netlify.com/`
-  );
-}
+core.setOutput(
+  'url',
+  pullRequestNumber
+    ? `https://${basicAuth}deploy-preview-${pullRequestNumber}--${siteName}.netlify.com/`
+    : `htttps://${basicAuth}${siteName}.netlify.com/`
+);
